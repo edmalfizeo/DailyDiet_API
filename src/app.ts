cd0054a9d6f386589fcde05206849b2b01ce1b05
edmalfizeo <edmalfizeo@gmail.com>
2024-12-05 14:58:11 -0300

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import { fastify } from "fastify";
+
+const app = fastify();
+
+
+
+app.listen( { port: 3000 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
